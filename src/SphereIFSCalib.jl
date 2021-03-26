0ba7397e26e416da43da5a1272a8f494bec92cb6
ferreol <ferreol.soulez@free.fr>
2021-03-26 11:34:01 +0100

    Comment code

--- src/SphereIFSCalib.jl
+++ src/SphereIFSCalib.jl
@@ -2,12 +2,18 @@
 using Zygote
 using TwoDimensional
 
+# Dispersion model
 struct DispModel
     λ::Float64   # reference wavelength
     order::Int32
 end
 
-function XYpos(self::DispModel,cx::Array{Float64,1},cy::Array{Float64,1})
+"""
+    (x,y) = (self::DispModel)(cx,cy) 
+    compute the position (x,y) given by the dispersion law wavelength DispModel.λ 
+    using the polynmial interpolation of order DispModel.order and coefficients (cx,cy) .
+"""
+function (self::DispModel)(cx::Array{Float64,1},cy::Array{Float64,1})
     x= cx[1];
     y= cy[1];
     for o in 2:self.order
@@ -17,34 +23,23 @@ function XYpos(self::DispModel,cx::Array{Float64,1},cy::Array{Float64,1})
     return (x,y)
 end
 
-XYpos(self::DispModel,C::Array{Float64,2}) = XYpos(self,C[1,:],C[2,:])
+"""
+    (x,y) = (self::DispModel)(C) 
+    compute the position (x,y) given by the dispersion law wavelength DispModel.λ 
+    using the polynmial interpolation of order DispModel.order and coefficients (cx=C[1,:],cy=C[2,:]).
+    C is an array of size (2,order)
+"""
+(self::DispModel)(C::Array{Float64,2}) = self(C[1,:],C[2,:])
 
-# wavelengths
-const λ1 = 987.72e-9# laser 1 
-const λ2 = 1123.71e-9# laser 2 
-const λ3 = 1309.37e-9# laser 3
-const λ4 = 1545.10e-9  # laser 4  
-const λ0 = λ1# reference
-
-# polynomial law  order
-order = 2
-
-laser1pos = DispModel(λ1-λ0,order)
-laser2pos = DispModel(λ2-λ0,order)
-laser3pos = DispModel(λ3-λ0,order)
-laser4pos = DispModel(λ4-λ0,order)
 
 function  GaussianModel(A::Float64,fwhm::Float64,x::Float64,y::Float64)
     local fwhm2sigma = 1/ (2*sqrt(2*log(2.)))::Float64
     return A * exp( -(x^2 + y^2) / (2 * (fwhm * fwhm2sigma )^2));
 end
 
-
-
-# Example
-f(a,fwhm,x) = GaussianModel(a,fwhm,XYpos(laser1pos,x)...)
-Cinit = rand(Float64,2,order);
-a0= 1.0;
-fwhm0= 1.0;
-f(a0,fwhm0,Cinit);
-gradient(f,a0,fwhm0,Cinit)
+function GaussianSpot(data::Array{Float64,2},weight::Array{Float64,2},bbox::BoundingBox{Int},A::Float64,fwhm::Float64,x::Float64,y::Float64)
+    for I in CartesianIndices(bbox)
+        m[I] = GaussianModel(A, fwhm, I[1] -x, I[2] -y);
+    end
+    return m
+end
--- test/PIC-test.jl
+++ test/PIC-test.jl
@@ -1,6 +1,21 @@
 
+# wavelengths
+const λ1 = 987.72e-9# laser 1 
+const λ2 = 1123.71e-9# laser 2 
+const λ3 = 1309.37e-9# laser 3
+const λ4 = 1545.10e-9  # laser 4  
+const λ0 = λ1# reference
+
+# polynomial law  order
+order = 2
+
+laser1pos = DispModel(λ1-λ0,order)
+laser2pos = DispModel(λ2-λ0,order)
+laser3pos = DispModel(λ3-λ0,order)
+laser4pos = DispModel(λ4-λ0,order)
+
 # Example
-f(a,fwhm,x) = GaussianModel(a,fwhm,XYpos(laser1pos,x)...)
+f(a,fwhm,x) = GaussianModel(a,fwhm,laser1pos(x)...)
 Cinit = rand(Float64,2,order);
 a0= 1.0;
 fwhm0= 1.0;
