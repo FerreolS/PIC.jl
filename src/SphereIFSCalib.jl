2c1e8595071305714568684b91a04ed1dc95b7e3
ferreol <ferreol.soulez@free.fr>
2021-03-26 18:00:33 +0100

    Recode everything to have a model per lenslet

--- src/SphereIFSCalib.jl
+++ src/SphereIFSCalib.jl
@@ -2,44 +2,116 @@
 using Zygote
 using TwoDimensional
 
+
 # Dispersion model
-struct DispModel
-    λ::Float64   # reference wavelength
-    order::Int32
+mutable struct DispModel
+    λ0::Float64   # reference wavelength
+    order::Int32  # order of the polynomial
+    cx::Array{Float64,1} # coefficients of the polynomial along the x axis
+    cy::Array{Float64,1} # coefficients of the polynomial along the y axis
+end
+
+# Model of a lenslet
+struct LensletModel
+    bbox::BoundingBox{Int}  # Boundingbox of influence of the lenslet on the detector
+    dmodel::DispModel       # dispersion model of the lenslet
+    λlaser::Array{Float64,1}# wavelength of the laser 
 end
 
 """
-    (x,y) = (self::DispModel)(cx,cy) 
-    compute the position (x,y) given by the dispersion law wavelength DispModel.λ 
-    using the polynmial interpolation of order DispModel.order and coefficients (cx,cy) .
+    lmod = LensletModel(λ0,λlaser,bbox);
+    Lenslet model constructor
+    * λ0 is the reference wavelength
+    * λlaser is a 1D array of laser wavelengths
+    * bbox is the bounding box of the lenslet on the detector 
 """
-function (self::DispModel)(cx::Array{Float64,1},cy::Array{Float64,1})
-    x= cx[1];
-    y= cy[1];
-    for o in 2:self.order
-        x += cx[o]  * self.λ^(o);
-        y += cy[o]  * self.λ^(o);
+function LensletModel(λ0::Float64, λlaser::Array{Float64,1}, bbox::BoundingBox{Int})
+    local order = length(λlaser) - 1; # the order of the polynomial is the number of laser -1
+    cx = zeros(Float64, order + 1); # coefficients of the polynomial along the x axis
+    cy = zeros(Float64, order + 1); # coefficients of the polynomial along the x axis
+    LensletModel(bbox, DispModel(λ0, order, cx, cy), λlaser)
+end
+
+"""
+    (x,y) = (self::DispModel)(λ) 
+    compute the position (x,y)  of the wavelent λ 
+    according to the dispersion law DispModel.
+"""
+function (self::DispModel)(λ::Float64)
+    x = self.cx[1];
+    y = self.cy[1];
+    for o in 1:self.order
+        x += self.cx[o + 1]  * (self.λ0 - λ)^(o);
+        y += self.cy[o + 1]  * (self.λ0 - λ)^(o);
     end
-    return (x,y)
+    return (x, y)
 end
 
 """
-    (x,y) = (self::DispModel)(C) 
-    compute the position (x,y) given by the dispersion law wavelength DispModel.λ 
-    using the polynmial interpolation of order DispModel.order and coefficients (cx=C[1,:],cy=C[2,:]).
-    C is an array of size (2,order)
+    D = update(D,C)
+    Update the coefficients C of the DispModel D.
 """
-(self::DispModel)(C::Array{Float64,2}) = self(C[1,:],C[2,:])
-
-
-function  GaussianModel(A::Float64,fwhm::Float64,x::Float64,y::Float64)
-    local fwhm2sigma = 1/ (2*sqrt(2*log(2.)))::Float64
-    return A * exp( -(x^2 + y^2) / (2 * (fwhm * fwhm2sigma )^2));
+function update(self::DispModel, C::Array{Float64,2})
+    self.cx = C[1,:];
+    self.cy = C[2,:];
+    return self
 end
 
-function GaussianSpot(data::Array{Float64,2},weight::Array{Float64,2},bbox::BoundingBox{Int},A::Float64,fwhm::Float64,x::Float64,y::Float64)
+"""
+     GaussianModel(A,fwhm,x,y)    
+    Compute the value  at position (x,y) of a 2D Gaussian of full-width at half xmaximum 
+    Update the coefficients C of the DispModel D.
+"""
+function GaussianModel(A::Float64, fwhm::Float64, x::Float64, y::Float64)
+    local fwhm2sigma = 1 / (2 * sqrt(2 * log(2.)))::Float64
+    return A * exp(-(x^2 + y^2) / (2 * (fwhm * fwhm2sigma )^2));
+end
+
+"""
+     GaussianModel(A,fwhm,x,y)    
+    Compute the value of a 2D Gaussian of full-width at half xmaximum 
+    Update the coefficients C of the DispModel D.
+"""
+function GaussianModel(A::Float64, fwhm::Float64, x::Array{Float64,2}, y::Array{Float64,2})
+    local fwhm2sigma = 1 / (2 * sqrt(2 * log(2.)))::Float64
+    return A .* exp.(.-(x.^2 + y.^2) ./ (2 * (fwhm * fwhm2sigma )^2));
+end
+
+"""
+    GaussianSpotsCost(data,weight,lmodel, A,fwhm,C)    
+    Compute a quadratic cost for the lenslet modelized by lmodel with parameters A fwhm and C
+"""
+function GaussianSpotsCost(data::Array{Float64,2}, weight::Array{Float64,2}, lmodel::LensletModel, A::Array{Float64,1}, fwhm::Array{Float64,1}, C::Array{Float64,2})
+    update(lmodel.dmodel, C);
+    s = 0.;
     for I in CartesianIndices(bbox)
-        m[I] = GaussianModel(A, fwhm, I[1] -x, I[2] -y);
+        spotsmodel = 0;
+        for (index, λ) in enumerate(lmodel.λlaser) 
+            (mx, my)  = lmodel.dmodel(λ);
+            spotsmodel += GaussianModel(A[index], fwhm[index], I[1] - mx, I[2] - my)  
+        end    
+        s += weight[I] * ( data[I] - spotsmodel)^2;
     end
-    return m
+    return s;
 end
+
+
+"""
+    GaussianSpotsModel(model,lmodel, A,fwhm,C)    
+    Build the model of the lenslet modelized by lmodel with parameters A fwhm and c
+"""
+function GaussianSpotsModel(model::Array{Float64,2}, lmodel::LensletModel, A::Array{Float64,1}, fwhm::Array{Float64,1}, C::Array{Float64,2})
+    update(lmodel.dmodel, C);
+    t = Zygote.Buffer(model);
+    t[:] = model[:];
+    for I in CartesianIndices(bbox)
+        spotsmodel = 0;
+        for (index, λ) in enumerate(lmodel.λlaser) 
+            (mx, my)  = lmodel.dmodel(λ);
+            t[I[1],I[2]] += GaussianModel(A[index], fwhm[index], I[1] - mx, I[2] - my)  
+        end
+    end
+    return Zygote.copy(t);
+end
+
+
--- test/PIC-test.jl
+++ test/PIC-test.jl
@@ -1,3 +1,6 @@
+include("../src/SphereIFSCalib.jl")
+
+using TwoDimensional, Zygote
 
 # wavelengths
 const λ1 = 987.72e-9# laser 1 
@@ -5,19 +8,21 @@ const λ2 = 1123.71e-9# laser 2
 const λ3 = 1309.37e-9# laser 3
 const λ4 = 1545.10e-9  # laser 4  
 const λ0 = λ1# reference
+const λlaser = [λ1,λ2,λ3,λ4]
 
-# polynomial law  order
-order = 2
+# model of the LensletModel
+bbox = BoundingBox(xmin=1, ymin=1, xmax=10, ymax=10);
+lmod = LensletModel(λ0,λlaser,bbox);
 
-laser1pos = DispModel(λ1-λ0,order)
-laser2pos = DispModel(λ2-λ0,order)
-laser3pos = DispModel(λ3-λ0,order)
-laser4pos = DispModel(λ4-λ0,order)
+data = rand(100,100)
+weight = Float64.(rand(100,100).>0.1);
 
-# Example
-f(a,fwhm,x) = GaussianModel(a,fwhm,laser1pos(x)...)
-Cinit = rand(Float64,2,order);
-a0= 1.0;
-fwhm0= 1.0;
-f(a0,fwhm0,Cinit);
-gradient(f,a0,fwhm0,Cinit)
+
+Cinit = rand(Float64,2,4);
+a0= rand(Float64,4);
+fwhm0= rand(Float64,4);
+
+likelihood(a,fwhm,C) = GaussianSpotsCost(data,weight,lmod,a,fwhm,C)
+cost = likelihood(a0,fwhm0,Cinit)
+
+∇cost = gradient(likelihood,a0,fwhm0,Cinit)
