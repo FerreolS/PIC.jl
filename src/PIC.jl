a864082abd22da32fa4a71e2756a7b4dddac15a6
ferreol <ferreol.soulez@free.fr>
2021-05-10 18:44:01 +0200

    Making a package

---
+++ src/PIC.jl
@@ -0,0 +1,11 @@
+# Copyright (c) 2021 Ferréol Soulez
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+module PIC
+
+include("SphereIFSCalib.jl")
+import .SphereIFSCalib
+
+end # module
